b43d80736341f3cefe67d29c062d7e7c6a1a9ccd
Éric Ivan João António <ericantoniodev@gmail.com>
2023-02-04 21:56:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="flex-container">
+
+      <div className="flex-child magenta">
+        Flex Column 1
+      </div>
+      
+      <div className="flex-child green">
+        Flex Column 2
+      </div>
+  
+  </div>
   );
 }
 
