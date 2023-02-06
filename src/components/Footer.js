1f22cd317c12a42dd508f3bf69386913b320fdb6
Éric Ivan João António <ericantoniodev@gmail.com>
2023-02-06 13:13:27 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer(){
     return(
-        <div>
+        <div className="footer--page">
             <footer>
                 <p>Oi</p>
             </footer>
