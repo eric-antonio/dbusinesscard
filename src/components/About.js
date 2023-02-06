25e7af707977cb9c25435607888d6e069a269598
Éric Ivan João António <ericantoniodev@gmail.com>
2023-02-06 13:01:29 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,10 @@ export default function About(){
     return(
         <div className="div--about">
             <h3>About</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat facilis, est illum autem recusandae neque culpa assumenda accusamus eligendi consectetur quam inventore possimus corporis rem necessitatibus natus eaque eum ad!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat facilis.</p>
 
             <h3>Interests</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat facilis, est illum autem recusandae neque culpa assumenda accusamus eligendi consectetur quam inventore possimus corporis rem necessitatibus natus eaque eum ad!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat facilis.</p>
         </div>
     )
 }
